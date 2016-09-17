5cff2dbc29d5a7e81279b74694b0198e85bda752
minu <minusarraf96@gmail.com>
2016-09-18 00:42:07 +0545

    total

---
+++ app/src/main/java/com/cab/alington/cab/splashscreen/TransitionGenerator.java
@@ -0,0 +1,10 @@
+package com.cab.alington.cab.splashscreen;
+
+import android.graphics.RectF;
+
+/**
+ * Created by abc on 9/16/2016.
+ */
+public interface TransitionGenerator {
+    public Transition generateNextTransition(RectF drawableBounds, RectF viewport);
+}
