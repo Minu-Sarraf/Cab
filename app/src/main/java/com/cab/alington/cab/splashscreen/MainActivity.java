bd5de61fd4621a02abcc3edaf0a378d42f23f2c5
minu <minusarraf96@gmail.com>
2016-09-26 16:20:07 +0545

    drawable

--- app/src/main/java/com/cab/alington/cab/splashscreen/MainActivity.java
+++ app/src/main/java/com/cab/alington/cab/splashscreen/MainActivity.java
@@ -128,9 +128,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             super.onBackPressed();
         }
     }
-
-
-    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
